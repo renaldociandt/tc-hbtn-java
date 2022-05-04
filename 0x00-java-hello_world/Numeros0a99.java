ba4367fa5b23f3eed0d259df8e1f7a1efbef5df5
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-04 08:19:15 -0700

    Number 0-99 file

---
+++ 0x00-java-hello_world/Numeros0a99.java
@@ -0,0 +1,16 @@
+public class Numeros0a99{
+
+	public static void main(String [] args){
+
+		int num = 0;
+		while (num < 100){
+			System.out.print(num + ", ");
+			if (num == 99){
+				System.out.println(num);
+			}
+			num++;
+		
+		}
+	}
+
+}
