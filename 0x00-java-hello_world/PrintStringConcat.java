f6f821e8d97c26fab1b610d74451dbbe812f0f37
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-03 18:04:44 -0700

    task a java

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,7 @@
+public class PrintStringConcat { public static void main(String[] args) 
+    {
+        String texto1 = "Holberton"; String texto2 = " School"; String 
+        texto3 = texto1 + texto2; System.out.println("Bem vindo a " + 
+        texto3 + "!");
+    }
+}
