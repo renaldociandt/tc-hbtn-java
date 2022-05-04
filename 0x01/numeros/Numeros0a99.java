59e154839094dc76bc4bbefd206b2d1896793a40
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-04 08:41:32 -0700

    atualização

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -3,13 +3,13 @@ public class Numeros0a99{
 	public static void main(String [] args){
 
 		int num = 0;
-		while (num < 100){
+		while (num < 99){
 			System.out.print(num + ", ");
+			num++;
 			if (num == 99){
 				System.out.println(num);
 			}
-			num++;
-		
+
 		}
 	}
 
