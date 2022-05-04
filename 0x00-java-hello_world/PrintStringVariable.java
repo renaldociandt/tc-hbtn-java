e97358e92705c47727514024b56409450348a811
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-03 17:47:43 -0700

    create substring

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -3,6 +3,5 @@ public class PrintStringVariable{ public static void main(String[] args)
         String text = "Holberton School"; System.out.println(text); 
         System.out.println(text); System.out.println(text.substring(0, 
         9));
-    
     }
 }
