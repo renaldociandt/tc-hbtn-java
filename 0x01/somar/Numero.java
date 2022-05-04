db45ddb2544ea465ee4803afa741658e8cf99406
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-04 16:15:36 -0700

    Numeros pares criado arquivo

---
+++ 0x01/somar/Numero.java
@@ -0,0 +1,5 @@
+public final class Numero { public static int somar(int a, int b) { int 
+        somar = a + b; return somar;
+        // escreva seu código aqui
+    }
+}
---
+++ 0x01/somar/Program.java
@@ -0,0 +1,6 @@
+public class Program { public static void main(String[] args) { Numero 
+        numero = new Numero(); System.out.printf("%d\n", 
+        Numero.somar(24, 32)); System.out.printf("%d\n", Numero.somar(5, 
+        0)); System.out.printf("%d\n", Numero.somar(4, -2));
+    }
+}
