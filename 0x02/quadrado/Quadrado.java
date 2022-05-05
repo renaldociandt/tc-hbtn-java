32871f23cb216b0c9eb82cffe889245d7ff522a6
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-05 12:51:24 -0700

    Program quadrado create

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,13 @@
+public final class Quadrado{ 
+	public static double area(double lado) {
+        
+        	if(lado < 0)
+ 
+			throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo");
+            
+            
+		return lado*lado;
+        }
+
+       
+}
