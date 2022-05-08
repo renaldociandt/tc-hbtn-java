296f514e8f3fddd0051d8ee9e2aacc21bdb7f4f8
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-08 15:23:18 -0300

    "created file program task conta bancaria especial"

---
+++ 0x03/contas_especiais/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,9 @@
+package contas_especiais.exceptions;
+
+public class OperacaoInvalidaException extends Exception{
+
+    public OperacaoInvalidaException(String msg){
+        super(msg);
+    }
+
+}
