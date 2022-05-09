eb45c163d9cf2f3b0ca36612c2517b3a227b0462
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-09 20:17:57 -0300

    task 3 morto e vivo (status)

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,48 @@
+
+
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public void tomarDano(int quantidadeDeDano){
+        if (this.getSaudeAtual() - quantidadeDeDano < 0) {
+            this.setSaudeAtual(0);
+        }else {
+            this.setSaudeAtual(this.getSaudeAtual() - quantidadeDeDano);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        if ((this.getSaudeAtual() + quantidadeDeCura) > 100){
+            this.setSaudeAtual(100);
+        }else {
+            this.setSaudeAtual(this.getSaudeAtual() + quantidadeDeCura);
+        }
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0){
+            this.status = "vivo";
+        }else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
