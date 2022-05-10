a6713dcd9fd7e6acb799cd17773b61f3860da705
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-10 20:48:31 -0300

    atualização class program

--- 0x05/quadrado/Program.java
+++ 0x05/quadrado/Program.java
@@ -1,16 +1,29 @@
-package quadrado;
 
 public class Program {
-
     public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado();
 
-            Retangulo retangulo = new Retangulo();
-            retangulo.setLargura(7);
-            retangulo.setAltura(4);
+        quadrado.setLado(10);
 
-            System.out.printf("Area: %.2f\n", retangulo.area());
-            System.out.println(retangulo.toString());
+        if (Retangulo.class.isAssignableFrom(Quadrado.class))
+            System.out.println("Quadrado eh uma subclasse de Retangulo");
+        else
+            System.out.println("Quadrado nao eh uma subclasse de Retangulo");
 
+        try {
+            Field campoAltura = Retangulo.class.getDeclaredField("altura");
+            int modifierAltura = campoAltura.getModifiers();
+            System.out.printf("Campo altura eh %s\n", Modifier.toString(modifierAltura));
+
+            Field campoLargura = Retangulo.class.getDeclaredField("altura");
+            int modifierLargura = campoLargura.getModifiers();
+            System.out.printf("Campo largura eh %s\n", Modifier.toString(modifierLargura));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.printf("Lado: %.2f\n", quadrado.getLado());
+        System.out.printf("Area: %.2f\n", quadrado.area());
+        System.out.printf(quadrado.toString() + "\n");
     }
-
 }
