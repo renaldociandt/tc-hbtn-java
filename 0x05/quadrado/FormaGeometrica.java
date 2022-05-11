b929f0877d5cd69c50b46ed5b398836a4a088b38
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-10 21:05:41 -0300

    atualização class

--- 0x05/quadrado/FormaGeometrica.java
+++ 0x05/quadrado/FormaGeometrica.java
@@ -1,11 +1,8 @@
-
+package quadrado;
 
 public class FormaGeometrica {
-
     public double area(){
-
 
         throw new  UnsupportedOperationException("Funcao area nao implementada");
     }
-
 }
