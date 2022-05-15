01142debc4098d7cae52eaa8c5e39f881eccca59
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-15 17:11:04 -0300

    task agregação

---
+++ 0x09/agregacao/Endereco.java
@@ -0,0 +1,77 @@
+
+
+public class Endereco {
+    private String logrador;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public Endereco(String logrador, String numero, String complemento, String bairro, String cidade, String estado, String pais) {
+        this.logrador = logrador;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+    }
+
+    public String getLogrador() {
+        return logrador;
+    }
+
+    public void setLogrador(String logrador) {
+        this.logrador = logrador;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
