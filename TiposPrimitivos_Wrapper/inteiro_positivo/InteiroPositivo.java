b820d69c25499cd660e073c53bbd42c54129b4ff
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-17 12:50:38 -0300

    Task tipos primitivos e wrap

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,54 @@
+
+
+public class InteiroPositivo {
+
+    public int valor;
+
+    public InteiroPositivo(int valor) {
+        this.valor = valor;
+    }
+
+    public InteiroPositivo(String valor) {
+
+        if (Integer.parseInt(valor) > 0){
+            this.setValor(Integer.parseInt(valor));
+
+        }else {
+            exception();
+        }
+
+    }
+
+    public boolean ehPrimo(){
+        int count = 0;
+        for (int x = 1; x <= valor; x++) {
+            if (valor % x == 0){
+                count += 1;
+            }
+        }
+        if (count == 2){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if (valor > 0){
+            this.valor = valor;
+
+        }else {
+            exception();
+        }
+
+    }
+
+    public void exception(){
+        throw new IllegalArgumentException ("Valor nao eh um valor inteiro positivo");
+    }
+
+}
