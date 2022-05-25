0ae82dd1bb9b7dd134ce326e2ba9531a08580ed7
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-25 15:19:32 -0300

    task 0 - Map - atualização

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -1,11 +1,12 @@
 
+
 import java.util.TreeMap;
 
 public class AnalisadorFrase {
     public static TreeMap<String, Integer> contagemPalavras(String palavra){
         TreeMap<String, Integer> map = new TreeMap<>();
 
-        String textoFormatar = palavra.replaceAll("[?.!]", " ");
+        String textoFormatar = palavra.replaceAll("[?.!]", "");
         String[] textoFormatado = textoFormatar.toLowerCase().split(" ");
 
         for (String texto : textoFormatado) {
