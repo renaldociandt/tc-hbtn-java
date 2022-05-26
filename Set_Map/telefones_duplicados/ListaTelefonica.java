16a34e4a42925eb08331ecfafdb4b5a025a87785
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-26 11:15:49 -0300

    task 3 - Map

---
+++ Set_Map/telefones_duplicados/ListaTelefonica.java
@@ -0,0 +1,39 @@
+package telefones_duplicados;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ListaTelefonica {
+    HashMap<String, HashSet<Telefone>> map;
+
+    public ListaTelefonica() {
+        this.map = new HashMap<String, HashSet<Telefone>>();
+    }
+
+    public HashSet<Telefone> adicionarTelefone(String nome, Telefone telefone){
+
+        HashSet<Telefone> tel = new HashSet<>();
+        tel.add(telefone);
+        if (buscar(nome) != null) {
+            if (map.containsKey(nome) && map.get(nome).contains(telefone)) {
+                throw new IllegalArgumentException("Telefone jah existente para essa pessoa");
+            }
+            for (String chave : map.keySet()) {
+                if (map.get(chave).contains(telefone)) {
+                    throw new IllegalArgumentException("Telefone jah pertence a outra pessoa");
+                }
+
+            }map.get(nome).add(telefone);
+        }else {
+            map.put(nome, tel);
+        }
+        return map.get(nome);
+    }
+
+    public HashSet<Telefone> buscar(String nome) {
+        if (map.containsKey(nome)) {
+            return map.get(nome);
+        }
+        return null;
+    }
+}
---
+++ Set_Map/telefones_duplicados/Telefone.java
@@ -0,0 +1,74 @@
+package telefones_duplicados;
+
+import java.util.Objects;
+
+public class Telefone {
+
+    private String codigoArea;
+    private String numero;
+
+    public Telefone(String codigoArea, String numero) {
+        this.codigoArea = codigoArea;
+        this.numero = numero;
+    }
+
+    public String getCodigoArea() {
+        return codigoArea;
+    }
+
+    public void setCodigoArea(String codigoArea) {
+        this.codigoArea = codigoArea;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString(){
+
+        return "(" + codigoArea + ") " + numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Telefone)) return false;
+
+        Telefone telefone = (Telefone) o;
+
+        if (!codigoArea.equals(telefone.codigoArea)) return false;
+        return numero.equals(telefone.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = codigoArea.hashCode();
+        result = 5 * result + numero.hashCode();
+        return result;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Telefone)) return false;
+//
+//        Telefone telefone = (Telefone) o;
+//
+//        if (!codigoArea.equals(telefone.codigoArea)) return false;
+//        return numero.equals(telefone.numero);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int result = 1;
+//        result = 5 * result + (codigoArea == null ? 0: codigoArea.hashCode());
+//
+//        result = 5 * result + (numero == null ? 0: numero.hashCode());
+//        return result;
+//    }
+}
