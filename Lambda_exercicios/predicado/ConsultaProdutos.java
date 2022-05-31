3e23305c08adf5daae97e4847d0dc4c069cc1185
Renaldo Abreu <renaldoc@ciandt.com>
2022-05-31 15:28:28 -0300

    task 1 - Lambida exercicio de fixação

--- Lambda_exercicios/predicado/ConsultaProdutos.java
+++ Lambda_exercicios/predicado/ConsultaProdutos.java
@@ -2,13 +2,14 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ConsultaProdutos {
 
-    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro){
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> produtoPredicate){
         List<Produto> produtoList = new ArrayList<>();
         for (Produto produto: produtos) {
-            if (criterioFiltro.testar(produto)){
+            if (produtoPredicate.test(produto)){
                 produtoList.add(produto);
 
             }
